2db4dcbff38ed6c3285860629ed89051fcca1b2d
griffjbudde <griffonjbudde@gmail.com>
2025-04-28 16:45:13 -0600

    Add gameboard function and game logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let getPlayers = function () {
-    player1 = prompt("Please enter the name of player one.")
-    player2 = prompt("Please enter the name of player two.")
+    let inputOne = document.querySelector("#player-one");
+    let inputTwo = document.querySelector("#player-two");
+    player1 = inputOne.value;
+    player2 = inputTwo.value;
     return player1, player2;
 };
 
@@ -19,67 +21,84 @@ let makeGameboard = function () {
     return gameboard;
 }();
 
-let playerOneChoice = function () {
-    choice = prompt(`${player1}, please make your pick`)
-    if (gameboard[choice] != 0) {
-        choice = prompt("already picked, pick again");
-        return gameboard[choice] = 1;
-        } else {
-    return gameboard[choice] = 1;
-    }
-}
-
-let playerTwoChoice = function () {
-    choice = prompt(`${player2}, please make your pick`)
-    if (gameboard[choice] != 0) {
-    choice = prompt("already picked, pick again");
-    return gameboard[choice] = 2;
-    } else {
-    return gameboard[choice] = 2;
-    }
-}
 
 let winChecker = function () {
     if (gameboard.a1 === gameboard.a2 && gameboard.a2 === gameboard.a3 && gameboard.a1 + gameboard.a2 + gameboard.a3 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.b1 === gameboard.b2 && gameboard.b2 === gameboard.b3 && gameboard.b1 + gameboard.b2 + gameboard.b3 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.c1 === gameboard.c2 && gameboard.c2 === gameboard.c3 && gameboard.c1 + gameboard.c2 + gameboard.c3 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.a1 === gameboard.b1 && gameboard.b1 === gameboard.c1 && gameboard.a1 + gameboard.b1 + gameboard.c1 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.a2 === gameboard.b2 && gameboard.b2 === gameboard.c2 && gameboard.a2 + gameboard.b2 + gameboard.c2 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.a3 === gameboard.b3 && gameboard.b3 === gameboard.c3 && gameboard.a3 + gameboard.b3 + gameboard.c3 > 0) {
-        alert("gameover")
         return gameOver = true;
     } else if (gameboard.a1 === gameboard.b2 && gameboard.b2 === gameboard.c3 && gameboard.a1 + gameboard.b2 + gameboard.c3 > 0) {
-        alert("gameover")
         return gameOver = true;
-    } else if (gameboard.a3 === gameboard.b2 && gameboard.b2 === gameboard.c3 && gameboard.a3 + gameboard.b2 + gameboard.c1 > 0) {
-        alert("gameover")
+    } else if (gameboard.a3 === gameboard.b2 && gameboard.b2 === gameboard.c1 && gameboard.a3 + gameboard.b2 + gameboard.c1 > 0) {
         return gameOver = true;
     } else {
         return gameOver = false;
     }
 }
 
-let playRound = function () {
-    playerOneChoice();
-    winChecker();
-    playerTwoChoice();
-    winChecker();
-    console.log(gameboard);
-};
 
-let game = function () {
-    gameOver = false;
-    while (gameOver === false) {
-        playRound();
-    }
-};
+let initializeButtons = function () {
+    const newButton = document.querySelector("#new-button");
+    const startButton = document.querySelector('#start-game-button');
+    const modal = document.querySelector(".modal");
+    
+    newButton.addEventListener('click', () => {
+        modal.style.display = "flex";    
+    })
+    startButton.addEventListener('click', () => {
+        modal.style.display = "none";
+        getPlayers();
+        gameOver = false;
+        playerTurn = player1;
+    })
+}();
+
+let initiliazeBoard = function () {
+    const gamesquare = document.getElementsByClassName('gamesquare');
+    var clickedSquare;
+    let counter = 0
+    for (let i = 0; i < gamesquare.length; i++)
+    gamesquare[i].addEventListener('click', () => {
+            if (playerTurn === player1 && gameOver === false) {
+                clickedSquare = gamesquare[i].id;
+                clickedSquareStat = gamesquare[i].textContent;
+                if (clickedSquareStat == '') {
+                    gamesquare[i].textContent = 'X';
+                    gameboard[clickedSquare] = 1; 
+                    counter++;
+                } else {
+                    playerTurn = player2;
+                }
+            } else if (playerTurn === player2 && gameOver === false) {
+                clickedSquare = gamesquare[i].id;
+                clickedSquareStat = gamesquare[i].textContent;
+                if (clickedSquareStat == '') {
+                    gamesquare[i].textContent = 'o';
+                    gameboard[clickedSquare] = 2;
+                    counter++;
+                } else {
+                    playerTurn = player1;
+                }
+            }
+            gamesquare[i].classList.add('picked');
+            winChecker()
+            console.log(counter)
+            console.log(playerTurn)
+            if (gameOver === true) {
+                alert('gameover')
+            } else if (counter == 9) {
+                alert('tie');
+                gameOver = true;
+            } else if (gameOver === false) {
+                playerTurn = (playerTurn == player1) ? player2 : player1;
+            }    
+    })
+}();
