335746c882f494196781b7388449f3293b150541
griffjbudde <griffonjbudde@gmail.com>
2025-04-28 17:18:04 -0600

    Add reset function

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # tic-tac-toe
 This is a tic tac toe game application. It is made using HTML, CSS,
-and the game logic is made using JavaScript.
+and the game logic is made using JavaScript. I used no global variables and 
+as many IIFEs as I could. I started by creating the game logic in JavaScript, but 
+I quickly found I relied too much on prompts stopoping the code and ran into countless
+problems when I tried to give it a UI. I found I needed to approach the game logic based
+off each click of the user iterating the steps of the game.
--- app.js
+++ app.js
@@ -95,11 +95,41 @@ let initiliazeBoard = function () {
             console.log(playerTurn)
             if (gameOver === true) {
                 display.textContent = `${playerTurn} wins! Click here to restart.`
+                counter = 0;
             } else if (counter == 9) {
                 gameOver = true;
                 display.textContent = 'Tie! Click here to restart.'
+                counter = 0;
             } else if (gameOver === false) {
                 playerTurn = (playerTurn == player1) ? player2 : player1;
             }    
+    })
+}();
+
+let initializeRestartButton = function () {
+    const restartBtn = document.querySelector('.display');
+    const gamesquare = document.getElementsByClassName('gamesquare');
+    const modal = document.querySelector(".modal");
+    restartBtn.addEventListener('click', () => {
+        for (let i = 0; i < gamesquare.length; i++) {
+            gamesquare[i].textContent = "";
+            gamesquare[i].classList.remove('picked');
+        }
+        restartBtn.textContent = "";
+        player1 = ""
+        player2 = "";
+        modal.style.display = 'flex';
+        gameboard = {
+            a1: 0,
+            a2: 0,
+            a3: 0,
+            b1: 0,
+            b2: 0,
+            b3: 0,
+            c1: 0,
+            c2: 0,
+            c3: 0,
+        }
+        return gameboard;
     })
 }();
